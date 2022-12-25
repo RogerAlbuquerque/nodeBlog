e6c3701ec3d8e6fefe0bd0e2b889edc828eead63
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-12-25 00:58:20 -0300

    Schema and collection for categori OK

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
   const handlebars = require('express-handlebars');
   const bodyParser = require('body-parser');
   const app        = express();
-  const admin      = require("./routes/admin");
+  const admin      = require('./routes/admin');
   const path       = require('path');
-  // const mongoose   = require('mongoose');
+  const mongoose   = require('mongoose') ;
 
 
 // Configurações
@@ -18,7 +18,9 @@
     app.set('view engine', 'handlebars');
 
   //Mongoose
-
+    mongoose.set("strictQuery", true);
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://127.0.0.1/nodeblog').then(()=>{console.log("conectou")}).catch((erro)=>{console.log("deu merda: " + erro)})
 
   //Public
     app.use(express.static(path.join(__dirname, "public")));  // Essa linha de código está indicado para o express onde fica a pasta que está guardado os arquivos estáticos desse projeto, que no caso é na pasta "public"
---
+++ models/Categori.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+//Definindo o model
+  const Categori = new Schema({
+
+    nome:{
+      type:String,
+      require:true
+    },
+
+    slug:{
+      type:String,
+      require:true
+    },
+
+    date:{
+      type:Date,
+      default:Date.now()
+    }
+
+  })
+
+//Collection
+mongoose.model('categories', Categori);
