52499d199148a40d3d0f22ae2b423cbf0179a446
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-12-26 14:47:21 -0300

    validating form to create categories

--- routes/admin.js
+++ routes/admin.js
@@ -26,15 +26,41 @@ router.post('/categories/new', (req,res)=>{
 
     var error = [];
     if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
+
       error.push({text: "Nome inválido"})
+
     }
+    if(req.body.name.length < 2){
+
+      error.push({text: "Nome da categoría muito pequeno"})
+
+    }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+
+      error.push({text: "Slug inválido"})
+
+    }
+
+     if(error.length > 0) {
+
+      res.render("admin/addcategories", {error: error})   // Aqui está passando parâmetros para o arquivo handlebars
+
+    }
+    else{
    
   const newCategori ={
     name: req.body.name,
     slug: req.body.slug
   }
 
-  new Categori(newCategori).save().then(()=> res.send("Nova categoria adicionada com sucesso"))
+  new Categori(newCategori).save().then(()=> {
+    req.flash("success_msg", "Categoria criada com sucesso" )
+    res.redirect("/admin/categories")})
+    .catch((err) =>{
+      req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!" )
+      res.redirect("/admin/categories")
+    })
+}
 });
 
 
